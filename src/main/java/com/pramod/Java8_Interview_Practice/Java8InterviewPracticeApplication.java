157579a973376b9e6912a259085bdb146c2447fb
Pramod <pramodgsable@gmail.com>
2024-11-11 10:57:31 +0530

    Adding Initial Spring Setup

---
+++ src/main/java/com/pramod/Java8_Interview_Practice/Java8InterviewPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.pramod.Java8_Interview_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8InterviewPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8InterviewPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pramod/Java8_Interview_Practice/Java8InterviewPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramod.Java8_Interview_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8InterviewPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
