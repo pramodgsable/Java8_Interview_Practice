4cf55a68d761ce9fd9ac6ee07c3cd4117d092a33
Pramod <pramodgsable@gmail.com>
2024-11-11 10:59:07 +0530

    Deleted unformatted files

--- com.pramod.java8/EvenOddSeparation.java
+++
@@ -1,8 +0,0 @@
-public class EvenOddSeparation {
-    public static void main(String[] args) {
-        List<Integer> listOfIntegers = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
-        for (int x : listOfIntegers){
-            System.out.println(x);
-        }
-    }
-}
